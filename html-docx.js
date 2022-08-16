c81aabe48ba681db109573eab04f5bb14bd145a0
Peter Rust <peter@cornerstonenw.com>
2022-08-16 11:05:38 -0700

    Inlined templates

--- html-docx.js
+++ html-docx.js
@@ -1,80 +1,9 @@
 let JSZip = require('jszip');
 let fs = require('fs');
 var _ = require('underscore');
+_.merge = require('lodash.merge');
 
 let html_docx = {asBlob, getMHTdocument, _prepareImageParts, generateDocument, addFiles, renderDocumentFile};
-
-let documentTemplate = _.template(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<w:document
-  xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main"
-  xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math"
-  xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships"
-  xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing"
-  xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main"
-  xmlns:ns6="http://schemas.openxmlformats.org/schemaLibrary/2006/main"
-  xmlns:c="http://schemas.openxmlformats.org/drawingml/2006/chart"
-  xmlns:ns8="http://schemas.openxmlformats.org/drawingml/2006/chartDrawing"
-  xmlns:dgm="http://schemas.openxmlformats.org/drawingml/2006/diagram"
-  xmlns:pic="http://schemas.openxmlformats.org/drawingml/2006/picture"
-  xmlns:ns11="http://schemas.openxmlformats.org/drawingml/2006/spreadsheetDrawing"
-  xmlns:dsp="http://schemas.microsoft.com/office/drawing/2008/diagram"
-  xmlns:ns13="urn:schemas-microsoft-com:office:excel"
-  xmlns:o="urn:schemas-microsoft-com:office:office"
-  xmlns:v="urn:schemas-microsoft-com:vml"
-  xmlns:w10="urn:schemas-microsoft-com:office:word"
-  xmlns:ns17="urn:schemas-microsoft-com:office:powerpoint"
-  xmlns:odx="http://opendope.org/xpaths"
-  xmlns:odc="http://opendope.org/conditions"
-  xmlns:odq="http://opendope.org/questions"
-  xmlns:odi="http://opendope.org/components"
-  xmlns:odgm="http://opendope.org/SmartArt/DataHierarchy"
-  xmlns:ns24="http://schemas.openxmlformats.org/officeDocument/2006/bibliography"
-  xmlns:ns25="http://schemas.openxmlformats.org/drawingml/2006/compatibility"
-  xmlns:ns26="http://schemas.openxmlformats.org/drawingml/2006/lockedCanvas">
-  <w:body>
-    <w:altChunk r:id="htmlChunk" />
-    <w:sectPr>
-      <w:pgSz w:w="<%= width %>" w:h="<%= height %>" w:orient="<%= orient %>" />
-      <w:pgMar w:top="<%= margins.top %>"
-               w:right="<%= margins.right %>"
-               w:bottom="<%= margins.bottom %>"
-               w:left="<%= margins.left %>"
-               w:header="<%= margins.header %>"
-               w:footer="<%= margins.footer %>"
-               w:gutter="<%= margins.gutter %>"/>
-    </w:sectPr>
-  </w:body>
-</w:document>
-`);
-
-let mhtDocumentTemplate = _.template(`MIME-Version: 1.0
-Content-Type: multipart/related;
-    type="text/html";
-    boundary="----=mhtDocumentPart"
-
-
-------=mhtDocumentPart
-Content-Type: text/html;
-    charset="utf-8"
-Content-Transfer-Encoding: quoted-printable
-Content-Location: file:///C:/fake/document.html
-
-<%= htmlSource %>
-
-<%= contentParts %>
-
-------=mhtDocumentPart--
-`);
-
-let mhtPartTemplate = _.template(`------=mhtDocumentPart
-Content-Type: <%= contentType %>
-Content-Transfer-Encoding: <%= contentEncoding %>
-Content-Location: <%= contentLocation %>
-
-<%= encodedContent %>
-`);
-
-_ = {merge: require('lodash.merge')};
 
 function asBlob(html, options) {
   let zip = new JSZip();
@@ -163,7 +92,48 @@ function renderDocumentFile(documentOptions) {
     })(), {
     margins: documentOptions.margins
     });
-    return documentTemplate(templateData);
+    return _.template(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<w:document
+    xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main"
+    xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math"
+    xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships"
+    xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing"
+    xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main"
+    xmlns:ns6="http://schemas.openxmlformats.org/schemaLibrary/2006/main"
+    xmlns:c="http://schemas.openxmlformats.org/drawingml/2006/chart"
+    xmlns:ns8="http://schemas.openxmlformats.org/drawingml/2006/chartDrawing"
+    xmlns:dgm="http://schemas.openxmlformats.org/drawingml/2006/diagram"
+    xmlns:pic="http://schemas.openxmlformats.org/drawingml/2006/picture"
+    xmlns:ns11="http://schemas.openxmlformats.org/drawingml/2006/spreadsheetDrawing"
+    xmlns:dsp="http://schemas.microsoft.com/office/drawing/2008/diagram"
+    xmlns:ns13="urn:schemas-microsoft-com:office:excel"
+    xmlns:o="urn:schemas-microsoft-com:office:office"
+    xmlns:v="urn:schemas-microsoft-com:vml"
+    xmlns:w10="urn:schemas-microsoft-com:office:word"
+    xmlns:ns17="urn:schemas-microsoft-com:office:powerpoint"
+    xmlns:odx="http://opendope.org/xpaths"
+    xmlns:odc="http://opendope.org/conditions"
+    xmlns:odq="http://opendope.org/questions"
+    xmlns:odi="http://opendope.org/components"
+    xmlns:odgm="http://opendope.org/SmartArt/DataHierarchy"
+    xmlns:ns24="http://schemas.openxmlformats.org/officeDocument/2006/bibliography"
+    xmlns:ns25="http://schemas.openxmlformats.org/drawingml/2006/compatibility"
+    xmlns:ns26="http://schemas.openxmlformats.org/drawingml/2006/lockedCanvas">
+    <w:body>
+    <w:altChunk r:id="htmlChunk" />
+    <w:sectPr>
+        <w:pgSz w:w="<%= width %>" w:h="<%= height %>" w:orient="<%= orient %>" />
+        <w:pgMar w:top="<%= margins.top %>"
+                w:right="<%= margins.right %>"
+                w:bottom="<%= margins.bottom %>"
+                w:left="<%= margins.left %>"
+                w:header="<%= margins.header %>"
+                w:footer="<%= margins.footer %>"
+                w:gutter="<%= margins.gutter %>"/>
+    </w:sectPr>
+    </w:body>
+</w:document>
+`)(templateData);
 }
 
 function getMHTdocument(htmlSource) {
@@ -172,10 +142,24 @@ function getMHTdocument(htmlSource) {
     ref = _prepareImageParts(htmlSource), htmlSource = ref.htmlSource, imageContentParts = ref.imageContentParts;
     // for proper MHT parsing all '=' signs in html need to be replaced with '=3D'
     htmlSource = htmlSource.replace(/\=/g, '=3D');
-    return mhtDocumentTemplate({
-        htmlSource: htmlSource,
-        contentParts: imageContentParts.join('\n')
-    });
+    return _.template(`MIME-Version: 1.0
+Content-Type: multipart/related;
+    type="text/html";
+    boundary="----=mhtDocumentPart"
+
+
+------=mhtDocumentPart
+Content-Type: text/html;
+    charset="utf-8"
+Content-Transfer-Encoding: quoted-printable
+Content-Location: file:///C:/fake/document.html
+
+<%= htmlSource %>
+
+<%= contentParts %>
+
+------=mhtDocumentPart--
+`)({htmlSource: htmlSource, contentParts: imageContentParts.join('\n')});
 }
 
 function _prepareImageParts(htmlSource) {
@@ -188,12 +172,13 @@ function _prepareImageParts(htmlSource) {
         index = imageContentParts.length;
         extension = contentType.split('/')[1];
         contentLocation = "file:///C:/fake/image" + index + "." + extension;
-        imageContentParts.push(mhtPartTemplate({
-        contentType: contentType,
-        contentEncoding: contentEncoding,
-        contentLocation: contentLocation,
-        encodedContent: encodedContent
-        }));
+        imageContentParts.push( _.template(`------=mhtDocumentPart
+Content-Type: <%= contentType %>
+Content-Transfer-Encoding: <%= contentEncoding %>
+Content-Location: <%= contentLocation %>
+
+<%= encodedContent %>
+`)({contentType, contentEncoding, contentLocation, encodedContent}));
         return "\"" + contentLocation + "\"";
     };
     if (typeof htmlSource === 'string') {
